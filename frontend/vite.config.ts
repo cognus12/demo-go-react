8ba814ef04e2397ab73f06d416d8c7aa0eaf85e7
Ivan Zotov <corvuss90@gmail.com>
2022-06-08 21:30:38 +0300

    feat: dev server proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,4 +13,14 @@ export default defineConfig({
       },
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000/api',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    }
+  }
 })
